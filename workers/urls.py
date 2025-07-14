d96e847c35874a6dd658f4b10f944a726785508b
ppieniazek <patryk.pieniazek2001@gmail.com>
2025-07-14 18:20:29 +0200

    asyncio test

--- workers/urls.py
+++ workers/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.test, name="test"),
+    path("time-stream/", views.time_stream, name="time_stream"),
 ]
--- workers/views.py
+++ workers/views.py
@@ -1,5 +1,20 @@
+import asyncio
+from datetime import datetime
+
+from datastar_py.django import DatastarResponse
+from datastar_py.django import ServerSentEventGenerator as SSE
 from django.shortcuts import render
 
 
 def test(request):
     return render(request, "workers/test.html")
+
+
+async def time_stream(request):
+    async def event_stream():
+        while True:
+            now = datetime.now().strftime("%H:%M:%S")
+            yield SSE.patch_signals({"currentTime": now})
+            await asyncio.sleep(1)
+
+    return DatastarResponse((event_stream()))
